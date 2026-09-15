5626ca362386e7703286ae77c57fd2ac56fb2483
Jonas Baethke <jonasb@post.com>
2026-09-15 06:53:57 +0200

    ok

--- glutin/build.rs
+++ glutin/build.rs
@@ -4,20 +4,38 @@ fn main() {
     // Setup alias to reduce `cfg` boilerplate.
     cfg_aliases! {
         // Systems.
+        trueos_platform: { target_os = "trueos" },
         android_platform: { target_os = "android" },
         ohos_platform: { target_env = "ohos" },
         wasm_platform: { target_family = "wasm" },
         macos_platform: { target_os = "macos" },
         ios_platform: { target_os = "ios" },
         apple: { any(ios_platform, macos_platform) },
-        free_unix: { all(unix, not(apple), not(android_platform), not(ohos_platform)) },
+        free_unix: {
+            all(
+                unix,
+                not(apple),
+                not(android_platform),
+                not(ohos_platform),
+                not(trueos_platform),
+            )
+        },
 
         // Native displays.
         x11_platform: { all(feature = "x11", free_unix, not(wasm_platform)) },
         wayland_platform: { all(feature = "wayland", free_unix, not(wasm_platform)) },
 
         // Backends.
-        egl_backend: { all(feature = "egl", any(windows, unix), not(apple), not(wasm_platform)) },
+        trueos_backend: { all(feature = "trueos", trueos_platform) },
+        egl_backend: {
+            all(
+                feature = "egl",
+                any(windows, unix),
+                not(apple),
+                not(wasm_platform),
+                not(trueos_platform),
+            )
+        },
         glx_backend: { all(feature = "glx", x11_platform, not(wasm_platform)) },
         wgl_backend: { all(feature = "wgl", windows, not(wasm_platform)) },
         cgl_backend: { all(macos_platform, not(wasm_platform)) },
